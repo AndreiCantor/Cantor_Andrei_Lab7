872ad2312034ece8de5cdd4dcf271124f32338a7
Cantor Andrei <cantorandrei18@gmail.com>
2023-11-26 12:04:01 +0200

    Add project files.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Cantor_Andrei_Lab7;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
